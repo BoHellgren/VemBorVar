4bbcde069df6626f57e09a355599689f0b10efd2
BoHellgren <bo.hellgren@gmail.com>
2017-12-21 17:30:56 +0100

    Skeleton

---
+++ src/pages/show-floor/show-floor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ShowFloorPage } from './show-floor';
+
+@NgModule({
+  declarations: [
+    ShowFloorPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ShowFloorPage),
+  ],
+})
+export class ShowFloorPageModule {}
