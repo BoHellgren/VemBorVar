fb33b8897db7f29620612c4ce62f6bb02fc1ec3d
BoHellgren <bo.hellgren@gmail.com>
2017-12-29 10:50:33 +0100

    Fully working user interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { AngularFireDatabase } from 'angularfire2/database';
 
+import { MembersProvider } from '../../providers/members/members';
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // members: Array<{ lgh: string, lmv: string, membername: string }>;
+  stairs: string[] = ['6 trappor ', '5 trappor ', '4 trappor ', '3 trappor ', '2 trappor ', '1 trappa '];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private membersProvider: MembersProvider) {
 
+  }
+
+  ionViewDidLoad(): void {
+    this.membersProvider.loadMembers();
+   // this.members = this.membersProvider.members;
+  }
+
+  GotoShowFloor(event, trappHus, row) {
+
+    var mask: string;
+    var streetAddr: string;
+    var floorNum: number;
+
+    floorNum = +row.substr(0, 1);
+    floorNum++;
+    mask = trappHus + floorNum.toString();
+    this.navCtrl.push('ShowFloorPage', mask);
+  }
+
+  GotoSearchPage() {
+    this.navCtrl.push('SearchPage');
   }
 
 }
--- src/pages/show-floor/show-floor.ts
+++ src/pages/show-floor/show-floor.ts
@@ -1,25 +1,73 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ShowFloorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { MembersProvider } from '../../providers/members/members';
 
-@IonicPage()
+@IonicPage({
+  defaultHistory: ['HomePage']
+})
 @Component({
   selector: 'page-show-floor',
   templateUrl: 'show-floor.html',
 })
 export class ShowFloorPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  liveHere: Array<{ lgh: string, lmv: string, membername: string }>;
+  streetAddr: string; // E.g. Taxgatan 3
+  houseNumber: string; // 1 for Taxgatan 7, 2 for Taxgatan 3
+  floorNum: number; // E.g. 6 or 1
+  floorName: string; //E.g. 6 trappor or 1 trappa
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private membersProvider: MembersProvider) {
+    var mask = navParams.data; // First two digits of lgh-number
+    console.log('attr ', mask.attr);
+    if (mask.substring === undefined) return;
+    this.houseNumber = mask.substring(0, 1);
+    if (this.houseNumber == '1') { this.streetAddr = 'Taxgatan 7' } else { this.streetAddr = 'Taxgatan 3' };
+    this.floorNum = +(mask.substring(1, 2)) - 1;
+    if (this.floorNum == 1) { this.floorName = this.floorNum.toString().concat(' trappa') }
+    else { this.floorName = this.floorNum.toString().concat(' trappor') };
+    this.getItems(mask);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShowFloorPage');
+  }
+
+  GoBack(event) {
+    // Goto back to home page
+    this.navCtrl.pop();
+  }
+
+  GoUp(event) {
+    // Goto next higher floor
+    if (this.floorNum < 6 || this.floorNum < 7 && this.streetAddr === 'Taxgatan 3') {
+      this.floorNum++;
+      this.floorName = this.floorNum.toString().concat(' trappor');
+      var mask = this.houseNumber.concat((this.floorNum + 1).toString());
+      this.getItems(mask);
+    }
+  }
+
+  GoDown(event) {
+    // Goto next lower floor
+    if (this.floorNum > 1) {
+      this.floorNum--;
+      if (this.floorNum == 1) { this.floorName = this.floorNum.toString().concat(' trappa') }
+      else { this.floorName = this.floorNum.toString().concat(' trappor') };
+      var mask = this.houseNumber.concat((this.floorNum + 1).toString());
+      this.getItems(mask);
+
+    }
+  }
+
+  getItems(mask) {
+    console.log('mask: ', mask);
+    // this.members = this.navParams.data;
+    this.liveHere = this.membersProvider.members.filter((member) => {
+      return (member.lgh.substring(0, 2).indexOf(mask) > -1);
+    })
   }
 
 }
